c577881aafe7349e408b9c81e6ccfbd7a29d5405
unrealwork <unrealwork@gmail.com>
2018-10-17 12:59:34 +0300

    added simple tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import {configure, mount, render} from "enzyme";
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+configure({adapter: new Adapter()});
+
+describe('App', () => {
+
+    const wrapper = mount(<App/>);
+
+    test('renders without crashing', () => {
+        expect(wrapper).toBeDefined();
+    });
+
+    test('I can see a textarea element with a corresponding id="editor"', () => {
+        const editorArea = wrapper.find('textarea#editor');
+        expect(editorArea).toHaveLength(1);
+    });
+
+
+    test('I can see an element with a corresponding id="preview"', () => {
+        const editorArea = wrapper.find('#preview');
+        expect(editorArea).toHaveLength(1);
+    })
+
 });
