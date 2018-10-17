052bd8fd8600d04bcd7c2ec84b288088a0745f6e
unrealwork <unrealwork@gmail.com>
2018-10-17 10:28:31 +0300

    Updates

---
+++ precache-manifest.af8d8fb07b01ecd5015e38992cba6bbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e2cf94d5a5b01798290",
+    "url": "unrealwork.github.io/markdown-previewer/static/js/runtime~main.7e2cf94d.js"
+  },
+  {
+    "revision": "671abec37cae3343bb7f",
+    "url": "unrealwork.github.io/markdown-previewer/static/js/main.671abec3.chunk.js"
+  },
+  {
+    "revision": "72adf7dc52c3bb17d642",
+    "url": "unrealwork.github.io/markdown-previewer/static/js/1.72adf7dc.chunk.js"
+  },
+  {
+    "revision": "671abec37cae3343bb7f",
+    "url": "unrealwork.github.io/markdown-previewer/static/css/main.4495ec5e.chunk.css"
+  },
+  {
+    "revision": "72adf7dc52c3bb17d642",
+    "url": "unrealwork.github.io/markdown-previewer/static/css/1.2cfa584c.chunk.css"
+  },
+  {
+    "revision": "ec98600ae43162fa21988ab32eed9637",
+    "url": "unrealwork.github.io/markdown-previewer/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "unrealwork.github.io/markdown-previewer/precache-manifest.af8d8fb07b01ecd5015e38992cba6bbc.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("unrealwork.github.io/markdown-previewer/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
