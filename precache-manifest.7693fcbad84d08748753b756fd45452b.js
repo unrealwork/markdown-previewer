e86951fabd0b198284abcdf55c63fdece9a32ba5
unrealwork <unrealwork@gmail.com>
2018-10-17 10:33:35 +0300

    Updates

---
+++ precache-manifest.7693fcbad84d08748753b756fd45452b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b2c275135a06121482d1",
+    "url": "/markdown-previewer/static/js/runtime~main.b2c27513.js"
+  },
+  {
+    "revision": "3edea8f3dca838261f0f",
+    "url": "/markdown-previewer/static/js/main.3edea8f3.chunk.js"
+  },
+  {
+    "revision": "72adf7dc52c3bb17d642",
+    "url": "/markdown-previewer/static/js/1.72adf7dc.chunk.js"
+  },
+  {
+    "revision": "3edea8f3dca838261f0f",
+    "url": "/markdown-previewer/static/css/main.4495ec5e.chunk.css"
+  },
+  {
+    "revision": "72adf7dc52c3bb17d642",
+    "url": "/markdown-previewer/static/css/1.2cfa584c.chunk.css"
+  },
+  {
+    "revision": "c80170cbadc6095c2262faa05b33e526",
+    "url": "/markdown-previewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "unrealwork.github.io/markdown-previewer/precache-manifest.af8d8fb07b01ecd5015e38992cba6bbc.js"
+  "/markdown-previewer/precache-manifest.7693fcbad84d08748753b756fd45452b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("unrealwork.github.io/markdown-previewer/index.html", {
+workbox.routing.registerNavigationRoute("/markdown-previewer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
